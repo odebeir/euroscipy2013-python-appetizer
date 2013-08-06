808a3dcce0eac1bface9bf3b1c0d3bab7ac31f38
Olivier Debeir <odebeir@ulb.ac.be>
2013-08-06 17:41:05 +0200

    add python tutor in types nb

--- notebooks/python_tutor_nb_tools.py
+++ notebooks/python_tutor_nb_tools.py
@@ -4,10 +4,14 @@
 from IPython.core.display import HTML
 import urllib
 
-def disp_code(c,h=500,w=1024):
+def disp_code(c,h=500,w=1024,heap=False):
     """Display an interactive Python Tuto iframe the the given code
     c = source code to test (string)
     """
+    if heap:
+        heap = 'true'
+    else:
+        heap = 'false'
     fmt = """<iframe width="%(width)d" height="%(height)d" frameborder="0"\
 src="http://pythontutor.com/iframe-embed.html#\
 code=%(code)s\
@@ -18,7 +22,7 @@ code=%(code)s\
 &showOnlyOutputs=%(output_only)s\
 &py=%(py)d\
 &curInstr=%(curinst)d"> </iframe>"""
-    d = {"cumulative":False,"heap":False,"pointer":False,"textref":False,"output_only":False,"py":2,"curinst":0,"height":h,"width":w}
+    d = {"cumulative":False,"heap":heap,"pointer":False,"textref":False,"output_only":False,"py":2,"curinst":0,"height":h,"width":w}
     d["code"] = urllib.urlencode({"c":c.strip('\n')})[2:] #URL encode the code string (after removing leading \n)
     return HTML(fmt%d)
 
